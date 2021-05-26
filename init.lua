0ebeb1577587f4b256104682978dfbed876e909c
Werekracken <75177612+Werekracken@users.noreply.github.com>
2021-05-25 21:38:48 -0500

    reducing the number of 1H egos because there weren't enough 2H staffs dropping anymore (#1)
    Co-authored-by: werekracken <j33tuck@yahoo.com>

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ long_name = "More 1H Staves"
 short_name = "more1hstaves"
 for_module = "tome"
 version = {1,7,2}
-addon_version = {1,0,1}
+addon_version = {1,0,2}
 weight = 2000
 author = {"Werekracken"}
 tags = {"staff", "1h", "staves"}
@@ -16,11 +16,9 @@ Also slightly expands the number of staves available in staff stores to account 
 https://github.com/Werekracken/tome-more1hstaves
 
 ---Changelog
-v1.0.0
-Initial release
-
-v1.0.1
-Changed the new 1H staves proc bonus from 2.5 (which is normal for 2H staves) to 1.2 (which is normal for 1H staves).
+- v1.0.0 Initial release
+- v1.0.1 Changed the new 1H staves proc bonus from 2.5 (which is normal for 2H staves) to 1.2 (which is normal for 1H staves).
+- v1.0.2 Took a few egos out because when the higher level/rarity staves are created they randomly get egos and if any ego is a 1H then the staff is 1H, meaning that too many of the higher level/rarity staves were 1H. Fewer 1H egos means lower chances of a 1H ego so it should be more balanced now.
 ]]
 overload = false
 superload = false
